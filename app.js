c40c3466f02784ede3e46a4ce940d2a1547a84e6
JOEL-8899 <56861930+JOEL-8899@users.noreply.github.com>
2022-11-20 14:12:01 -0500

    Third commit

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from 'express';
 import HelloController from "./controllers/hello-controllers.js";
 import UserController from "./controllers/users/users-controller.js";
+import TuitsController from "./controllers/tuits/tuits-controller.js"
+import cors from 'cors';
 const app = express()
+app.use(cors());
+app.use(express.json());
 HelloController(app);
+TuitsController(app);
 UserController(app);
 app.listen(4000)
