f1602b3db1db9de550bde5c067c609292ec08af8
JOEL-8899 <56861930+JOEL-8899@users.noreply.github.com>
2022-11-27 19:33:53 -0500

    connected to the local mongoDB server and tuiter app working properly. Implemented the tuits-dao and changed the tuiter controller as intended.

--- app.js
+++ app.js
@@ -4,13 +4,17 @@ import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js"
 import cors from 'cors';
 import mongoose from "mongoose";
-mongoose.connect("mongodb://localhost:27017/tuiter");
+
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING
+    || 'mongodb://localhost:27017/tuiter';
+mongoose.connect(CONNECTION_STRING);
 
 const app = express()
 app.use(cors());
 app.use(express.json());
 HelloController(app);
 TuitsController(app);
+
 UserController(app);
 app.listen(process.env.PORT || 4000);
 
